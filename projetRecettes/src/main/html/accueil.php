c4d59d5c102d83743df5f6311eaa88f5f20bd739
gautier57 <gautierklam@gmail.com>
2021-11-08 17:32:55 +0100

    probleme encodage résolu

--- projetRecettes/src/main/html/accueil.php
+++ projetRecettes/src/main/html/accueil.php
@@ -5,13 +5,16 @@
     include("connexion.php");
     ?>
         <title>Accueil Recette</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     </head>
     <body>
     <?php
+
       $sth = $conn->query("SELECT * FROM recette");
+
       $sth->execute();
       while($row = $sth->fetch()) {
-        echo  $row['description'].' '.$row['introduction'].' '.$row['nom'].' '.$row['recette_type']."<br>";
+        echo  utf8_encode($row['description']).' '. utf8_encode($row['introduction']).' '.utf8_encode($row['nom']).' '.utf8_encode($row['recette_type'])."<br>";
       }
     ?>
     </body>
