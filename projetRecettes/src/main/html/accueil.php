fce906891c5ff54894fb303013c8363b403e1ef1
gautier57 <gautierklam@gmail.com>
2021-11-08 15:51:14 +0100

    commit test

--- projetRecettes/src/main/html/accueil.php
+++ projetRecettes/src/main/html/accueil.php
@@ -3,5 +3,9 @@
         <title>Accueil Recette<title>
     </head>
     <body>
+    <?php
+      $sth = $dbco->prepare("SELECT prenom, mail FROM users");
+      $sth->execute();
+    ?>
     </body>
 </html>
