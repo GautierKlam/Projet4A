708f173d9db480686ebb1548ffc969894f38c33f
Naphyu <Romain.Perrelle@outlook.fr>
2021-11-10 17:44:38 +0100

    ajout de méthodes utilisateur v2

--- projetRecettes/src/main/java/fr/polytech/projetrecettes/ProjetRecettesApplication.java
+++ projetRecettes/src/main/java/fr/polytech/projetrecettes/ProjetRecettesApplication.java
@@ -2,9 +2,11 @@ package fr.polytech.projetrecettes;
 
 import fr.polytech.projetrecettes.dao.AvisInterface;
 import fr.polytech.projetrecettes.dao.RecetteInterface;
+import fr.polytech.projetrecettes.dao.UtilisateurInterface;
 import fr.polytech.projetrecettes.entities.Avis;
 import fr.polytech.projetrecettes.entities.Recette;
 import fr.polytech.projetrecettes.entities.RecetteType;
+import fr.polytech.projetrecettes.entities.Utilisateur;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,11 +22,14 @@ public class ProjetRecettesApplication {
     }
 
     @Bean
-    CommandLineRunner start(RecetteInterface recetteInterface, AvisInterface avisInterface){
+    CommandLineRunner start(RecetteInterface recetteInterface, UtilisateurInterface utilisateurInterface, AvisInterface avisInterface){
         return args -> {
             avisInterface.save(new Avis(1,5,"Super recette, je recommande !"));
             avisInterface.save(new Avis(2,3.5,"Bravo, tout le monde a adoré !"));
             avisInterface.save(new Avis(3,1,"Pas fou, on sent à peine le goût de la viande..."));
+
+            utilisateurInterface.save(new Utilisateur(1,"Orlabite","Jade","Le canard","6969","OKLM@gmail.fr",true));
+            utilisateurInterface.save(new Utilisateur(2,"Kulfor","Jean","Le pigeon","6969","OKLM2@gmail.fr",true));
 
             recetteInterface.save(new Recette(1,"Salade de pâtes",4,"Très bonne recette, idéale pour l'été !","- Faites cuire vos pâtes\n- Mélangez les pâtes refroidies avec le basilic, le jambon, les tomates cerises, la roquette et la mozzarella\n- Ajoutez de l'huile et du vinaigre, salez, poivrez",new ArrayList<String>(){{add("200g");add("4 tranches");add("500g");add("100g");add("Quelques feuilles");}} ,new ArrayList<String>(){{add("Roquette");add("Jambon de pays");add("Tomates cerises");add("Mozzarella");add("Basilic");}}, RecetteType.PLAT,new ArrayList<Avis>(){{add(new Avis(1,7,"Effectivement très frais !"));add(new Avis(2,10,"rien à redire c'etait parfait"));}}));
             recetteInterface.save(new Recette(2,"Magret de canard",2,"Super","Description magret de canard",new ArrayList<String>(){{add("2");}},new ArrayList<String>(){{add("Magret de canard");}}, RecetteType.PLAT, new ArrayList<Avis>(){{add(new Avis(1,5,"Super recette, je recommande !"));}}));
