c87bb91241339e3fc8d0f729825dc16c733fdd12
gautier57 <gautierklam@gmail.com>
2021-11-10 16:31:28 +0100

    page d'accueil et entete presque fini

--- projetRecettes/src/main/html/accueil.php
+++ projetRecettes/src/main/html/accueil.php
@@ -5,17 +5,27 @@
     include("connexion.php");
     ?>
         <title>Accueil Recette</title>
+        	<link rel="stylesheet" href="accueil.css" type="text/css">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     </head>
+    <?php include ('entete.php');?>
     <body>
+      <h1>Bienvenue sur notre site de recette collaboratif !</h1>
+      <h2>Voici l'ensemble des recettes disponibles, cliquez sur le nom pour voir ses avis et sa description entière</h2>
     <?php
-
       $sth = $conn->query("SELECT * FROM recette");
-
       $sth->execute();
+      echo "<table>";
+      echo "<caption>Liste des recettes (nom,petite description et type)</caption>";
+
       while($row = $sth->fetch()) {
-        echo  utf8_encode($row['description']).' '. utf8_encode($row['introduction']).' '.utf8_encode($row['nom']).' '.utf8_encode($row['recette_type'])."<br>";
+          echo "<tr>";
+        $intr= utf8_encode($row['introduction']);
+        $nom=utf8_encode($row['nom']);
+        $typerecette=utf8_encode($row['recette_type']);
+        echo"<td>".$nom."</td><td>".$intr."</td><td>".$typerecette."</td></tr>";
       }
+        echo "</table>"
     ?>
     </body>
 </html>
