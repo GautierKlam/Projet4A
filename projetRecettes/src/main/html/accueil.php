baca5607a08aa53bf30b3aff27b0ebf1281ee5ac
gautier57 <gautierklam@gmail.com>
2022-01-09 22:10:48 +0100

    espace compte effectué

--- projetRecettes/src/main/html/accueil.php
+++ projetRecettes/src/main/html/accueil.php
@@ -1,7 +1,24 @@
 <?php session_start(); ?>
 <html>
     <head>
-        <?php include("connexion.php");?>
+        <?php include("connexion.php");
+        $admin=false;
+        if(isset($_SESSION['pseudo'])){
+            $pseu=$_SESSION['pseudo'];
+            $page1 = file_get_contents('http://localhost:8888/projetrecettes/utilisateurs');
+            $obj1 = json_decode($page1,true);
+            $test1 = $obj1['_embedded'];
+              foreach ($test1['utilisateurs'] as $m) {
+                if ($pseu==$m['pseudo']){
+                  if ($m['admin']==true){
+                    $admin=true;
+                  }else{
+                    $admin=false;
+                  }
+                }
+              }
+        }
+        ?>
         <title>Accueil Recette</title>
         <link rel="stylesheet" href="accueil.css" type="text/css">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -12,7 +29,7 @@
     <body>
         <h1>Bienvenue sur notre site de recette collaboratif !</h1>
         <h2 style='text-align:center'>Voici l'ensemble des recettes disponibles, cliquez sur le nom pour voir ses avis et sa description entière</h2>
-      
+
         <div style='text-align:center'>
             <form method='GET'>
                 <input size='40%' type='search' name='recherche' placeholder='Rechercher une recette...' />
@@ -53,8 +70,13 @@
                     $nom = $v['nom'];
                     $intr = $v['introduction'];
                     $typerecette = $v['recetteType'];
-                    echo"<tr><td><a href='voirrecette.php?id=$id'>".$nom."</a></td><td>".$intr."</td><td>".$typerecette."</td></tr>";
-                }  
+                    if ($admin==true){
+                      $phrase="<td><a href='modif.php'><img title='Modifier la recette' src='modifier.png' width='20px' ></a></td><td><a href='supprimer.php'><img title='Supprimer la recette' src='delete.png' width='20px' ></a></td>";
+                    }else{
+                      $phrase="";
+                    }
+                    echo"<tr><td><a href='voirrecette.php?id=$id'>".$nom."</td><td>".$intr."</td><td>".$typerecette."</td>".$phrase."</tr>";
+                }
                 echo "</table>";
             }
         ?>
