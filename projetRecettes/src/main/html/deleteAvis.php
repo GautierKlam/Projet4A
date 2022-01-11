4aad314ba3dea2af893eabfe996e771577689501
BernardelliStephane <steph.bernardelli@hotmail.fr>
2022-01-11 12:50:49 +0100

    Modif/suppression avis

--- projetRecettes/src/main/html/deleteAvis.php
+++ projetRecettes/src/main/html/deleteAvis.php
@@ -3,15 +3,26 @@
     if(!isset($_SESSION['pseudo'])){
         header('Location: accueil.php');
         exit;
-    }
+    }?>
 
+<html>
+    <head>
+        <?php include("connexion.php");?>
+        <title>Votre compte</title>
+        <link rel="stylesheet" href="menu.css" type="text/css">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    </head>
+</html>
+
+<?php
+    include('entete.php');
     $id = $_GET['id'];
     $id_avis = $_GET['id_avis'];
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "http://localhost:8888/deleteAvis");
     curl_setopt($ch, CURLOPT_POST, 1);
-    $datas = array("id_recette"=>$id, "id"=>$id_avis);
+    $datas = array("id_recette"=>$id, "id_avis"=>$id_avis);
     curl_setopt($ch, CURLOPT_POSTFIELDS,$datas);
     $result = curl_exec($ch);
     print_r($result);
