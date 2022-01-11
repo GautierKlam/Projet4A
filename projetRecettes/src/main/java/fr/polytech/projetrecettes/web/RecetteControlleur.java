110c31ac8e86efd9df416996d55a3c73155047ce
BernardelliStephane <steph.bernardelli@hotmail.fr>
2022-01-11 12:49:46 +0100

    Suppression méthodes inutiles

--- projetRecettes/src/main/java/fr/polytech/projetrecettes/web/RecetteControlleur.java
+++ projetRecettes/src/main/java/fr/polytech/projetrecettes/web/RecetteControlleur.java
@@ -24,28 +24,6 @@ public class RecetteControlleur {
         return (ArrayList<Recette>) recetteInterface.findAll();
     }
 
-    //renvoie la liste des ingrédients
-    @GetMapping(value = "/recettes/{identifiant}/ingredients")
-    public List<Ingredient> ingredientsList(@PathVariable int identifiant){
-        return recetteInterface.getById(identifiant).getIngredients();
-    }
-
-    //renvoie la liste des avis
-    @GetMapping(value = "/recettes/{identifiant}/avis")
-    public List<Avis> avisList(@PathVariable int identifiant){
-        return recetteInterface.getById(identifiant).getAvis();
-    }
-
-    //renvoie le nombre de recettes
-    private int taille() {
-        return recetteList().size();
-    }
-
-    //renvoie le nombre d'avis d'une recette
-    private int tailleAvis(Recette recette){
-        return recette.getAvis().size();
-    }
-
     //ajouter une recette
     @PostMapping("/saveRecette")
     public Recette saveRecette(Recette recette){
@@ -72,91 +50,7 @@ public class RecetteControlleur {
         return recetteInterface.save(recette);
     }
 
-    //chercher une recette à l'aide de son identifiant
-    @GetMapping("/recettes/{identifiant}")
-    public Recette getRecette(@PathVariable("identifiant")int id){
-        return recetteInterface.findById(id).get();
-    }
-
-    //Mettre à jour l'identifiant d'une recette
-    @PutMapping("/recettes/{id}/identifiant")
-    public Recette saveIntro(@PathVariable int id, int identifiant){
-        Recette recette = recetteInterface.getById(id);
-        recette.setId_recette(identifiant);
-        return recetteInterface.save(recette);
-    }
-
-    //Mettre à jour le nombre de personnes
-    @PutMapping("/{recette}/nb_personnes")
-    public Recette saveNb(@PathVariable Recette recette, int nb_personnes){
-        recette.setNb_personnes(nb_personnes);
-        return recetteInterface.save(recette);
-    }
-
-    //Mettre à jour le nom
-    @PutMapping("/{recette}/nom")
-    public Recette saveNom(@PathVariable Recette recette, String nom){
-        recette.setNom(nom);
-        return recetteInterface.save(recette);
-    }
-
-    //Mettre à jour l'introduction
-    @PutMapping("/{recette}/introduction")
-    public Recette saveIntro(@PathVariable Recette recette, String introduction){
-        recette.setIntroduction(introduction);
-        return recetteInterface.save(recette);
-    }
-
-    //Mettre à jour la description
-    @PutMapping("/{recette}/description")
-    public Recette saveDescription(@PathVariable Recette recette, String description){
-        recette.setDescription(description);
-        return recetteInterface.save(recette);
-    }
-
-    //Mettre à jour l'identifiant d'un ingredient
-    @PutMapping("/{recette}/{ingredient}/identifiant")
-    public Recette saveId(@PathVariable Recette recette, @PathVariable Ingredient ingredient, int identifiant){
-        recette.getIngredients().get(ingredient.getId_ingredient()-1).setId_ingredient(identifiant);
-        return recetteInterface.save(recette);
-    }
-
-    //Modifier le nom d'un ingrédient
-    @PutMapping("/{recette}/{ingredient}/nom")
-    public Recette saveNomIngredient(@PathVariable Recette recette, @PathVariable Ingredient ingredient, String nom){
-        recette.getIngredients().get(ingredient.getId_ingredient()-1).setNom(nom);
-        return recetteInterface.save(recette);
-    }
-
-    //Modifier la quantité d'un ingrédient
-    @PutMapping("/{recette}/{ingredient}/quantite")
-    public Recette saveQuantiteIngredient(@PathVariable Recette recette, @PathVariable Ingredient ingredient, String quantite){
-        recette.getIngredients().get(ingredient.getId_ingredient()-1).setQuantite(quantite);
-        return recetteInterface.save(recette);
-    }
-
-    //Mettre à jour le type
-    @PutMapping("/{recette}/type")
-    public Recette saveType(@PathVariable Recette recette, RecetteType type){
-        recette.setRecetteType(type);
-        return recetteInterface.save(recette);
-    }
-
-    //Mettre à jour l'identifiant d'un avis
-    @PutMapping("/{recette}/{avis}/identifiant")
-    public Recette saveId(@PathVariable Recette recette, @PathVariable Avis avis,  int identifiant){
-        recette.getAvis().get(avis.getId_avis()-1).setId_avis(identifiant);
-        return recetteInterface.save(recette);
-    }
-
-    //Modifier la note d'un avis
-    @PutMapping("/{recette}/{avis}/note")
-    public Recette saveNote(@PathVariable Recette recette, @PathVariable Avis avis, double note){
-        recette.getAvis().get(avis.getId_avis()-1).setNote(note);
-        return recetteInterface.save(recette);
-    }
-
-    //Modifier le commentaire d'un avis
+    //Modifier un avis
     @PostMapping("/modifAvis")
     public Recette modifAvis(int id_recette, String commentaire, int note, int id_avis){
         Recette recette = recetteInterface.getById(id_recette);
@@ -167,22 +61,10 @@ public class RecetteControlleur {
     }
 
     //suppression d'un avis
-    @DeleteMapping("/{recette}/avis/{identifiant}")
-    public Recette deleteAvis(@PathVariable Recette recette, @PathVariable("identifiant") int id){
-        recette.getAvis().remove(id);
+    @PostMapping("/deleteAvis")
+    public Recette deleteAvis(int id_recette, int id_avis){
+        Recette recette = recetteInterface.getById(id_recette);
+        recette.getAvis().remove(id_avis);
         return recetteInterface.save(recette);
-    }
-
-    //suppression d'un ingredient
-    @DeleteMapping("/{recette}/ingredients/{identifiant}")
-    public Recette deleteIngredient(@PathVariable Recette recette, @PathVariable("identifiant") int id){
-        recette.getIngredients().remove(id);
-        return recetteInterface.save(recette);
-    }
-
-    //suppression d'une recette
-    @DeleteMapping("/recettes/{identifiant}")
-    public void delete(@PathVariable("identifiant")int id){
-        recetteInterface.deleteById(id);
     }
 }
--- projetRecettes/src/main/java/fr/polytech/projetrecettes/web/UtilisateurControlleur.java
+++ projetRecettes/src/main/java/fr/polytech/projetrecettes/web/UtilisateurControlleur.java
@@ -17,76 +17,9 @@ public class UtilisateurControlleur {
         return (ArrayList<Utilisateur>) utilisateurInterface.findAll();
     }
 
-    //renvoie le nombre d'utilisateurs
-    private int taille() {
-        return utilisateurList().size();
-    }
-
     //ajouter un utilisateur
     @PostMapping("/saveUtilisateur")
     public Utilisateur saveUtilisateur(@ModelAttribute Utilisateur utilisateur){
         return utilisateurInterface.save(utilisateur);
-
-    }
-
-    //Rechercher un utilisateur par son identifiant
-    @GetMapping("/utilisateurs/identifiant")
-    public Utilisateur getUtilisateur(@PathVariable("identifiant")int id){
-        return utilisateurInterface.findById(id).get();
-    }
-
-    //Mise à jour identifiant
-    @PutMapping("/{utilisateur}/identifiant")
-    public Utilisateur saveId(@PathVariable Utilisateur utilisateur, @PathVariable int identifiant){
-        utilisateur.setIdentifiant(identifiant);
-        return utilisateurInterface.save(utilisateur);
-    }
-
-    //Mise à jour nom
-    @PutMapping("/{utilisateur}/nom")
-    public Utilisateur saveNom(@PathVariable Utilisateur utilisateur,@PathVariable String nom){
-        utilisateur.setNom(nom);
-        return utilisateurInterface.save(utilisateur);
-    }
-
-    //Mise à jour prénom
-    @PutMapping("/{utilisateur}/prenom")
-    public Utilisateur savePrenom(@PathVariable Utilisateur utilisateur,@PathVariable String prenom){
-        utilisateur.setPrenom(prenom);
-        return utilisateurInterface.save(utilisateur);
-    }
-
-    //Mise à jour pseudo
-    @PutMapping("/{utilisateur}/pseudo")
-    public Utilisateur savePseudo(@PathVariable Utilisateur utilisateur,@PathVariable String pseudo){
-        utilisateur.setPseudo(pseudo);
-        return utilisateurInterface.save(utilisateur);
-    }
-
-    //Mise à jour mdp
-    @PutMapping("/{utilisateur}/mdp")
-    public Utilisateur saveMdp(@PathVariable Utilisateur utilisateur,@PathVariable("mdp")String mdp) {
-        utilisateur.setPseudo(mdp);
-        return utilisateurInterface.save(utilisateur);
-    }
-
-    //Mise à jour mail
-    @PutMapping("/{utilisateur}/mail")
-    public Utilisateur saveMail(@PathVariable Utilisateur utilisateur,@PathVariable String mail){
-        utilisateur.setMail(mail);
-        return utilisateurInterface.save(utilisateur);
-    }
-
-    //Mise à jour admin
-    @PutMapping("/{utilisateur}/admin")
-    public Utilisateur saveAdmin(@PathVariable Utilisateur utilisateur,@PathVariable boolean admin){
-        utilisateur.setAdmin(admin);
-        return utilisateurInterface.save(utilisateur);
-    }
-
-    //suppression d'un utilisateur
-    @DeleteMapping("/utilisateurs/identifiant")
-    public void save(@PathVariable int identifiant){
-        utilisateurInterface.deleteById(identifiant);
     }
 }
